22174117be6029a4107a666503f91857efd30a28
Documenter.jl <documenter@juliadocs.github.io>
2022-04-17 15:22:08 +0000

    build based on 9454bc6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RadialBasisFiniteDifferences","category":"page"},{"location":"#RadialBasisFiniteDifferences","page":"Home","title":"RadialBasisFiniteDifferences","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RadialBasisFiniteDifferences.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RadialBasisFiniteDifferences]","category":"page"}]
+}
