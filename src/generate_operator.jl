9765a753783ef23e2b04310bafad29ee7f3c9564
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-08-08 09:04:57 -0400

    Convert indexing to generic indexing
    Change from using length to specific calls to indexing functions such as eachindex and lastindex

--- src/generate_operator.jl
+++ src/generate_operator.jl
@@ -19,19 +19,18 @@ function generate_operator(X, Y, p, n, polydeg)
     add_to_graph!(hnsw_x)
     # Find k (approximate) nearest neighbors for each of the queries
     idxs_x, dists_x = knn_search(hnsw_x, X, n)
-    idxs_x = [convert.(Int, idxs_x[x]) for x in 1:length(idxs_x)] # Convert to readable
+    idxs_x = [convert.(Int, idxs_x[x]) for x in eachindex(idxs_x)] # Convert to readable
     # Find single nearest neighbor for each Y point
     idxs_y_x, dists_y_x = knn_search(hnsw_x, Y, 1)
-    idxs_y_x = [convert.(Int, idxs_y_x[x]) for x in 1:length(idxs_y_x)] # Convert to readable
+    idxs_y_x = [convert.(Int, idxs_y_x[x]) for x in eachindex(idxs_y_x)] # Convert to readable
 
     ### Storing matrices and scale factors
-    m = length(X)
+    m = lastindex(X)
     M_int = Array{Matrix,2}(undef, (m, 2))
     scale = Array{SVector{2},1}(undef, m)
 
     ### Testing looping through all indeces of X 
-    for i in 1:length(X)
-
+    for i in eachindex(X)
         ### Add Shifting and Scaling of Local X Matrices
         X_shift, scale_x, scale_y = scalestencil(X[idxs_x[i]])
         scale[i] = [scale_x, scale_y]
@@ -44,8 +43,8 @@ function generate_operator(X, Y, p, n, polydeg)
     end
 
     ### Shift and Scale the Y points
-    Y_shift = Array{SVector{2},1}(undef, length(Y))
-    for i in 1:length(Y)
+    Y_shift = Array{SVector{2},1}(undef, lastindex(Y))
+    for i in eachindex(Y)
         #ad1 = [idxs_x[idxs_y_x[i]][1]][1][1]
         #ad2 = [idxs_x[idxs_y_x[i]][1]][1][2]
         Y_shift[i] = [(Y[i][1] - X[idxs_x[idxs_y_x[i]][1][1]][1]) *
@@ -59,13 +58,13 @@ function generate_operator(X, Y, p, n, polydeg)
 
     ### Need to perform several shifts in order to get the correct values for PHS
     # Evaluate over domain of Y
-    E_loc = zeros(length(Y), n)
-    Dx_loc = zeros(length(Y), n)
-    Dy_loc = zeros(length(Y), n)
-    Dxx_loc = zeros(length(Y), n)
-    Dyy_loc = zeros(length(Y), n)
-    Dxy_loc = zeros(length(Y), n)
-    for k in 1:length(Y)
+    E_loc = zeros(lastindex(Y), n)
+    Dx_loc = zeros(lastindex(Y), n)
+    Dy_loc = zeros(lastindex(Y), n)
+    Dxx_loc = zeros(lastindex(Y), n)
+    Dyy_loc = zeros(lastindex(Y), n)
+    Dxy_loc = zeros(lastindex(Y), n)
+    for k in eachindex(Y)
         # Take interpolation matrix from x in X which is closest to the current y in Y
         idx_inv = idxs_y_x[k]
         idx_inv_local = idxs_x[idx_inv][1][1]
@@ -77,11 +76,11 @@ function generate_operator(X, Y, p, n, polydeg)
         idx_c = k
 
         # Shift and scale the stencil nodes to a circle
-        Xscaled = Array{SVector{2},1}(undef, length(idx))
+        Xscaled = Array{SVector{2},1}(undef, lastindex(idx))
         Yscaled = Vector(undef, 2)
 
         # Shift the stencil nodes so that the center node is the origin
-        for i in 1:length(idx)
+        for i in eachindex(idx)
             Xscaled[i] = X[idx[i]] - X[idx_inv][1]
         end
 
@@ -93,14 +92,14 @@ function generate_operator(X, Y, p, n, polydeg)
         scale_y = scale[idx_inv][1][2]
 
         # Unroll scaling
-        for i in 1:length(idx)
+        for i in eachindex(idx)
             Xscaled[i] = [Xscaled[i][1] * scale_x, Xscaled[i][2] * scale_y]
         end
         Yscaled = [Yscaled[1] * scale_x, Yscaled[2] * scale_y]
 
         # Stencil distance matrices. Center point is in the origin (0)
-        stencil_coord = Array{SVector{2},1}(undef, length(idx))
-        for i in 1:length(idx)
+        stencil_coord = Array{SVector{2},1}(undef, lastindex(idx))
+        for i in eachindex(idx)
             dx_stencil = Yscaled[1] - Xscaled[i][1]
             dy_stencil = Yscaled[2] - Xscaled[i][2]
             if dx_stencil == 0
@@ -147,9 +146,9 @@ function generate_operator(X, Y, p, n, polydeg)
 
     ### Generate Sparse Matrices from Local Operator Matrices
     # From MATLAB Implementation
-    idx_rows = repeat((1:length(Y))', n)'
-    idx_columns = Array{Int64}(undef, length(Y), n) # Change to eltype of existing indices
-    for i in 1:length(Y)
+    idx_rows = repeat((eachindex(Y))', n)'
+    idx_columns = Array{Int64}(undef, lastindex(Y), n) # Change to eltype of existing indices
+    for i in eachindex(Y)
         idx_columns[i, :] = idxs_x[idxs_y_x[i]][1]
     end
     ### Convert to Generate Sparse Matrix 
