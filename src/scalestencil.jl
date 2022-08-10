0091dbe0c5ec36c1dc32397dd229d65151fd97e3
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-08-10 19:51:21 -0400

    Update scalestencil.jl to staticarrays

--- src/scalestencil.jl
+++ src/scalestencil.jl
@@ -3,20 +3,22 @@ function scalestencil(X)
     X_local = X
     # Shift to origin
     # Determine size for Distance Block
-    X_shift = Array{MVector,1}(undef, length(X_local)) # Rework to use SVector
+    X_shift = Array{SVector{2},1}(undef, lastindex(X_local)) # Rework to use SVector
+    X_ = Array{SVector{2},1}(undef, lastindex(X_local)) # Rework to use SVector
     # Pre-shift Values
     # Assuming first value is the current interpolation point
     x_shift = X_local[1]
     # X_shift
     for j in eachindex(X_local)
-        X_shift[j] = X_local[j] - x_shift
+        X_[j] = X_local[j] - x_shift
     end
 
     # Normalize vectors according to largest entry
-    scale_x = 1.0 / max([abs(X_shift[x][1]) for x in eachindex(X_shift)]...)
-    scale_y = 1.0 / max([abs(X_shift[x][2]) for x in eachindex(X_shift)]...)
-    [X_shift[x][1] = X_shift[x][1] * scale_x for x in eachindex(X_local)]
-    [X_shift[x][2] = X_shift[x][2] * scale_y for x in eachindex(X_local)]
+    scale_x = 1.0 / max([abs(X_[x][1]) for x in eachindex(X_)]...)
+    scale_y = 1.0 / max([abs(X_[x][2]) for x in eachindex(X_)]...)
+    for i in eachindex(X_local)
+        X_shift[i] = [X_[i][1] * scale_x, X_[i][2] * scale_y]
+    end
 
     return X_shift, scale_x, scale_y
 end
