0905638026845ca5856e24444e8685f6155f820f
jarias9 <68081892+jarias9@users.noreply.github.com>
2022-08-14 12:49:51 -0400

    Remove BC-to-BC influence
    Further partition computation to remove boundary point influence on each other. Now BC points only depend on interior points.

--- src/generate_operator.jl
+++ src/generate_operator.jl
@@ -181,41 +181,43 @@ function generate_operator(X, Y, p, n, polydeg, X_idx_in, X_idx_bc, X_idx_bc_g, 
     F, F_x, F_y, F_xx, F_yy, F_xy = polynomialbasis(polydeg, 2)
 
     # Generate KNN Tree Using HNSW 
+    ### Note: we can make it so that the influence of other boundaries 
+    ### is not present in BCs by evaluating NN using only interior points
+    ### then at the end add BCs to tree and eval NN of interior points
     #Intialize HNSW struct
     hnsw_x = HierarchicalNSW(X)
-    #Add all data points into the graph
     #Optionally pass a subset of the indices in data to partially construct the graph
     X_idx_in_bc = Array(X_idx_in)
     for i in eachindex(X_idx_bc)
         append!(X_idx_in_bc, X_idx_bc[i])
     end
-    # Graph does not contain ghosts
-    add_to_graph!(hnsw_x, X_idx_in_bc)
-    # Find k (approximate) nearest neighbors for each of the queries
-    idxs_x, dists_x = knn_search(hnsw_x, X, n)
-    #idxs_x = [convert.(Int, idxs_x[x]) for x in eachindex(idxs_x)] # Convert to readable
-    # Find single nearest neighbor for each Y point
-    idxs_y_x, dists_y_x = knn_search(hnsw_x, Y, 1)
-    #idxs_y_x = [convert.(Int, idxs_y_x[x]) for x in eachindex(idxs_y_x)] # Convert to readable
-    ### Separate according to element type
+    # Graph only contains interior points
+    add_to_graph!(hnsw_x, X_idx_in)
+    # Separate according to element type
+    # Calculate NN for each BC point but without
+    # including other BCs
     idxs_x = Array{SVector{n}}(undef, lastindex(X))
     dists_x = Array{SVector{n}}(undef, lastindex(X))
     idxs_y_x = Array{SVector{1}}(undef, lastindex(Y))
     dists_y_x = Array{SVector{1}}(undef, lastindex(Y))
+    # Calculate BC and ghost at the same time
+    # Remove 1 neighbor for ghost
+    for i in eachindex(X_idx_bc)
+        for j in eachindex(X_idx_bc[i])
+            idxs_local, dists_local = knn_search(hnsw_x, X[X_idx_bc[i][j]], n - 2)
+            idxs_x[X_idx_bc[i][j]] = [X_idx_bc[i][j]; idxs_local; X_idx_bc_g[i][j]]
+            idxs_x[X_idx_bc_g[i][j]] = [X_idx_bc_g[i][j]; X_idx_bc[i][j]; idxs_local]
+            dists_x[X_idx_bc[i][j]] = [0.0; dists_local; 0.0] #Note: 0.0 at end should be ghost offset
+            dists_x[X_idx_bc_g[i][j]] = [0.0; 0.0; dists_local] # Second 0.0 should be ghost offset
+        end
+    end
+    # Add BC points to graph
+    add_to_graph!(hnsw_x, X_idx_in_bc)
+    # Calculate weights for interior 
     for i in X_idx_in
         idxs_x[i], dists_x[i] = knn_search(hnsw_x, X[X_idx_in[i]], n)
     end
-    # Remove 1 neighbor for ghost
-    # Calculate BC and ghost at the same time
-    for i in eachindex(X_idx_bc)
-        for j in eachindex(X_idx_bc[i])
-            idxs_local, dists_local = knn_search(hnsw_x, X[X_idx_bc[i][j]], n - 1)
-            idxs_x[X_idx_bc[i][j]] = [idxs_local; X_idx_bc_g[i][j]]
-            idxs_x[X_idx_bc_g[i][j]] = [X_idx_bc_g[i][j]; idxs_local]
-            dists_x[X_idx_bc[i][j]] = [dists_local; 0.0] #Note: 0.0 should be ghost offset
-            dists_x[X_idx_bc_g[i][j]] = [0.0; dists_local]
-        end
-    end
+    # Add all points to graph
     # Calculate Y Nearest Neighbor
     add_to_graph!(hnsw_x)
     for i in eachindex(Y)
