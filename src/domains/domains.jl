7ef5ff4b7781017b4b926ea5a70b2c8cbdee46fd
jarias9 <68081892+jarias9@users.noreply.github.com>
2024-02-01 21:07:04 -0500

    Start of major refactor

---
+++ src/domains/domains.jl
@@ -0,0 +1,5 @@
+####################################################################################################
+# Include files with actual implementations for different types of meshfree discretizations
+
+# include("point_domain.jl")
+# include("cell_domain.jl")
